7bb95bdcf76d9a155e1affb247d717e4f5ef273a
Oleg Borodatov <borolgs@gmail.com>
2022-10-17 09:04:50 +0300

    tasks: implement user consumer

--- apps/tasks/src/kafka-app/kafka-app.controller.ts
+++ apps/tasks/src/kafka-app/kafka-app.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserCreatedEvent } from '../types';
 import { UsersConsumerService } from './user-consumer.service';
 
 @Controller()
 export class KafkaAppController {
   constructor(private userConsumer: UsersConsumerService) {}
+  @MessagePattern('account-stream')
+  async getUserMessage(@Payload() payload: UserCreatedEvent) {
+    this.userConsumer.consume(payload);
+  }
 }
